5e3bbc418a8a2f76b16bc26ea15c443620aa84b0
Balázs Bálint Balázs <b.balint642@gmail.com>
2023-05-07 18:56:07 +0200

    Adding and eventListener

--- script.js
+++ script.js
@@ -9,5 +9,7 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+  });
 }
